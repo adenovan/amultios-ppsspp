77d1809e9e40c0a1eb2710f15bc05e4f39d7ee81
Gde Made Novan Priambhada <adhenovan@gmail.com>
2017-11-11 04:38:29 +0800

    Amultios Custom Fork

--- UI/NativeApp.cpp
+++ UI/NativeApp.cpp
@@ -275,7 +275,7 @@ int NativeMix(short *audio, int num_samples) {
 
 // This is called before NativeInit so we do a little bit of initialization here.
 void NativeGetAppInfo(std::string *app_dir_name, std::string *app_nice_name, bool *landscape, std::string *version) {
-	*app_nice_name = "PPSSPP";
+	*app_nice_name = "Amultios";
 	*app_dir_name = "ppsspp";
 	*landscape = true;
 	*version = PPSSPP_GIT_VERSION;
