hrydgard/ppsspp

Add virtual key for opening chat

Common/KeyMap.h
@@ -51,6 +51,7 @@ enum {
 	VIRTKEY_AXIS_SWAP = 0x10012,
 	VIRTKEY_DEVMENU = 0x10013,
 	VIRTKEY_FRAME_ADVANCE = 0x10014,
+	VIRTKEY_OPENCHAT = 0x10015,
 	VIRTKEY_LAST,
 	VIRTKEY_COUNT = VIRTKEY_LAST - VIRTKEY_FIRST
 };

adenovan/amultios-ppsspp

Common/KeyMap.h
@@ -52,6 +52,7 @@ enum {
 	VIRTKEY_DEVMENU = 0x40000014,
 	VIRTKEY_FRAME_ADVANCE = 0x40000015,
 	VIRTKEY_RECORD = 0x40000016,
+	VIRTKEY_OPENCHAT = 0x40000017,
 	VIRTKEY_LAST,
 	VIRTKEY_COUNT = VIRTKEY_LAST - VIRTKEY_FIRST
 };